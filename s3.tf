65589ca5c62af2064798a2665f57d909db8ecc61
@slovacus <slovacus@gmail.com>
2020-07-10 20:35:44 -0500

    chore: change acl to public read (#5)

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "main" {
   provider      = aws.main
   bucket        = var.domain
-  acl           = "private"
+  acl           = "public-read"
   force_destroy = var.force_destroy
   website {
     redirect_all_requests_to = var.target
